314ba2d0de43178f8c177d7c59d389cb310b4142
sep121796 <sunilawasthi2000@gmail.com>
2023-12-06 21:29:27 +0530

    datatypes and variable

---
+++ datatypes.js
@@ -0,0 +1,4 @@
+let name ="sunil"
+let phoneno=9027431437
+let signinn=true
+console.log(typeofname)
---
+++ variable.js
@@ -0,0 +1,8 @@
+
+
+const accountid=123456789
+
+let email="sunilawasthi2000@gmail.com"
+var accountname="sunilawasthi"
+console.table([accountid,email,accountname])
+
