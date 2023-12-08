ab04b9e722ad139e01d249f2efa93f3b3ae91b33
sep121796 <sunilawasthi2000@gmail.com>
2023-12-08 08:36:02 +0530

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+const newarray=["sunil","sandeep","yash"]
+const oldarray=["sajal","sunny","tara"]
+const totalarray=newarray.concat(oldarray)
+
+const spred =[...newarray,...oldarray]
+
+console.log(spred)
