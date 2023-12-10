2cfaef2099b00372fde075c9e484d6304db89271
sep121796 <sunilawasthi2000@gmail.com>
2023-12-10 17:40:01 +0530

    loops

---
+++ loop.js
@@ -0,0 +1,17 @@
+
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element)
+    
+}
+
+
+for (let index = 0; index < 20; index++) {
+  
+    if(index==5){
+        console.log("detated 5");
+    }
+    console.log(`value of i is ${index}`)
+    
+}
